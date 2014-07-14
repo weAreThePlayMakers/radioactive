232f1ef07eb2effaa6eccd449ca154f0a551b5aa
@rafszul <rafszul@wearetheplaymakers.com>
2014-07-14 18:07:57 +0100

    adobeAn first published

---
+++ adobeAn/radioActive002-1/radioActive002-1_edgeActions.js
@@ -0,0 +1,28 @@
+
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${_Symbol_1}","click",function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${_Symbol_1}","dblclick",function(sym,e){});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'Symbol_1'
+(function(symbolName){})("Symbol_1");
+//Edge symbol end:'Symbol_1'
+
+//=========================================================
+
+//Edge symbol: 'lindsey'
+(function(symbolName){})("lindsey");
+//Edge symbol end:'lindsey'
+
+//=========================================================
+
+//Edge symbol: 'radioActive'
+(function(symbolName){})("radioActive");
+//Edge symbol end:'radioActive'
+})(jQuery,AdobeEdge,"EDGE-7283819");
